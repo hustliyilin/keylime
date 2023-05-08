b5aa65f01826cb1d14c0861fc04772fff2867dad
YiLin.Li <YiLin.Li@linux.alibaba.com>
2023-05-08 16:56:48 +0800

    docker/release: change the base image to alinux3
    Signed-off-by: YiLin.Li <YiLin.Li@linux.alibaba.com>

--- docker/release/build_locally.sh
+++ docker/release/build_locally.sh
@@ -9,6 +9,6 @@ KEYLIME_DIR=${2:-"../../"}
 
 ./generate-files.sh ${VERSION}
 for part in base registrar verifier tenant; do
-  docker buildx build -t keylime_${part}:${VERSION} -f ${part}/Dockerfile $KEYLIME_DIR --progress plain ${@:3}
+  docker buildx build -t keylime_${part}_alinux3:${VERSION} -f ${part}/Dockerfile $KEYLIME_DIR --progress plain ${@:3}
   rm -f ${part}/Dockerfile
 done
